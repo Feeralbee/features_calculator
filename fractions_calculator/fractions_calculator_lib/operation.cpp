f9eb69b558420fef9359d8ff03a5d4a1f96b63d3
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2022-03-11 19:58:18 +0300

    add cpp files

---
+++ fractions_calculator/fractions_calculator_lib/operation.cpp
@@ -0,0 +1,99 @@
+#include "operation.h"
+
+int Fractions::Greatest_common_divisor(int a, int b)
+{
+	if (b == 0) {
+		return a;
+	}
+	else {
+		return Greatest_common_divisor(b, a % b);
+	}
+}
+
+Fractions::Fractions()
+{
+	numerator = 0;
+	denominator = 1;
+}
+
+int Fractions::Get_numerator() const
+{
+	return numerator;
+}
+
+int Fractions::Get_denominator() const
+{
+	return denominator;
+}
+
+
+Fractions::Fractions(int new_numerator, int new_denominator)
+{
+	if (new_denominator == 0) {
+		throw std::invalid_argument("Invalid argument");
+	}
+	const int gcd = Greatest_common_divisor(new_numerator, new_denominator);
+	numerator = new_numerator / gcd;
+	denominator = new_denominator / gcd;
+	if (denominator < 0) {
+		denominator = -denominator;
+		numerator = -numerator;
+	}
+}
+
+
+Fractions operator + (const Fractions& lhs, const Fractions& rhs)
+{
+	return{
+		lhs.Get_numerator() * rhs.Get_denominator() + rhs.Get_numerator() * lhs.Get_denominator(),
+		lhs.Get_denominator() * rhs.Get_denominator()
+	};
+}
+
+Fractions operator - (const Fractions& lhs, const Fractions& rhs)
+{
+	return{
+		lhs.Get_numerator() * rhs.Get_denominator() - rhs.Get_numerator() * lhs.Get_denominator(),
+		lhs.Get_denominator() * rhs.Get_denominator()
+	};
+}
+
+Fractions operator * (const Fractions& lhs, const Fractions& rhs)
+{
+	return{
+		lhs.Get_numerator() * rhs.Get_numerator(),
+		lhs.Get_denominator() * rhs.Get_denominator()
+	};
+}
+
+Fractions operator / (const Fractions& lhs, const Fractions& rhs)
+{
+	if (lhs.Get_numerator() == 0) {
+		throw std::domain_error("Division by zero");
+	};
+	if (rhs.Get_numerator() == 0) {
+		throw std::domain_error("Division by zero");
+	};
+	return lhs * Fractions(rhs.Get_denominator(), rhs.Get_numerator());
+}
+
+std::istream& operator >> (std::istream& is, Fractions& r)
+{
+	int n, d;
+	char c;
+	is >> n >> c >> d;
+	if (is && c == '/') {
+		r = Fractions(n, d);
+	}
+	return is;
+}
+
+std::ostream& operator << (std::ostream& os, const Fractions& r)
+{
+	return os << r.Get_numerator() << '/' << r.Get_denominator();
+}
+
+bool operator < (const Fractions& lhs, const Fractions& rhs)
+{
+	return (lhs - rhs).Get_numerator() < 0;
+}
---
+++ fractions_calculator/fractions_calculator_lib/operation.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <stdexcept>
+#include <sstream>
+#include <fstream>
+
+
+class Fractions {
+public:
+	Fractions();
+	Fractions(int new_numerator, int new_denominator);
+
+	int Greatest_common_divisor(int a, int b);
+
+	int Get_numerator() const;
+	int Get_denominator() const;
+
+private:
+	int numerator;
+	int denominator;
+};
+
+Fractions operator + (const Fractions& lhs, const Fractions& rhs);
+
+Fractions operator - (const Fractions& lhs, const Fractions& rhs);
+
+Fractions operator * (const Fractions& lhs, const Fractions& rhs);
+
+Fractions operator / (const Fractions& lhs, const Fractions& rhs);
+
+std::istream& operator >> (std::istream& is, Fractions& r);
+
+std::ostream& operator << (std::ostream& os, const Fractions& r);
+
+bool operator < (const Fractions & lhs, const Fractions & rhs);
