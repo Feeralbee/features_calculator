fa84dbaa72d8707dc0af855e7751804c06be0178
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2022-03-13 15:03:17 +0300

    delete unnecessary cpp files

--- fractions_calculator/fractions_calculator_ui_test/notifications_tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-#include "notification.h"
-
-TEST(get_notifications_tests, get_notifications)
-{
-	ASSERT_EQ(get_notification(notification_index::welcome_phrase), "Hi! This is fraction calculator. Example of data entery: 1/2(first fraction) /(arithmetic action) 1/2(second fraction)");
-}
